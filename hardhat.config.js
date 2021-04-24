7098394e0214370d69125244c0728956dab0683e
dbehmoaras@gmail.com <dbehmoaras@gmail.com>
2021-04-24 15:31:45 -0400

    refactored with global vars

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,18 +1,22 @@
 /**
 * @type import('hardhat/config').HardhatUserConfig
 */
-require('dotenv').config();
-require("@nomiclabs/hardhat-ethers");
+const path = require('path');
+console.log(__dirname)
+require('dotenv').config({path: __dirname+'/alcheMeta.env'});
 const { API_URL, PRIVATE_KEY } = process.env;
-// console.log(API_URL)
+console.log(API_URL)
+
+require("@nomiclabs/hardhat-ethers");
+
 module.exports = {
    solidity: "0.7.3",
    defaultNetwork: "ropsten",
    networks: {
       hardhat: {},
       ropsten: {
-         url: "https://eth-ropsten.alchemyapi.io/v2/Q6DLGfQL7ycNsgHRkfQc3MrQmUXg6sR9",
-         accounts: ["0xdcd0b28ff35020f6d49f8e817f81397e829925abf6e7b1a1155cb7005f51fa19"]
+         url: API_URL,
+         accounts: [`0x${PRIVATE_KEY}`]
       }
    },
 }
--- scripts/mint-nft.js
+++ scripts/mint-nft.js
@@ -1,12 +1,20 @@
-require('dotenv').config();
+const path = require('path');
+console.log(__dirname)
+require('dotenv').config({path:'/home/david/Dropbox/HackCentral/ImmersiveGit/classRepo/scratchProj/CoinMaker/alcheMeta.env'});
 // const API_URL = process.env.API_URL;
+const { API_URL , PRIVATE_KEY , PUBLIC_KEY , IPFS_HASH_COHORT25 } = process.env;
+console.log(API_URL)
+
+
 const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
 const web3 = new createAlchemyWeb3("https://eth-ropsten.alchemyapi.io/v2/Q6DLGfQL7ycNsgHRkfQc3MrQmUXg6sR9");
 
-const API_URL = "https://eth-ropsten.alchemyapi.io/v2/Q6DLGfQL7ycNsgHRkfQc3MrQmUXg6sR9";
-const PRIVATE_KEY = "dcd0b28ff35020f6d49f8e817f81397e829925abf6e7b1a1155cb7005f51fa19";
-const PUBLIC_KEY = "0x7709c76de4add704c3ee81272f2b78b943ceb368";
-const IPFS_HASH_COHORT25 = "QmTtcFD3RqF4eRStSqRbYnoQ4MquEginASgrpPMzY9BadT";
+
+
+// const API_URL = "https://eth-ropsten.alchemyapi.io/v2/Q6DLGfQL7ycNsgHRkfQc3MrQmUXg6sR9";
+// const PRIVATE_KEY = "dcd0b28ff35020f6d49f8e817f81397e829925abf6e7b1a1155cb7005f51fa19";
+// const PUBLIC_KEY = "0x7709c76de4add704c3ee81272f2b78b943ceb368";
+// const IPFS_HASH_COHORT25 = "QmTtcFD3RqF4eRStSqRbYnoQ4MquEginASgrpPMzY9BadT";
 
 const contract = require("../artifacts/contracts/MyNFT.sol/MyNFT.json");
 // console.table(JSON.stringify(contract.abi));
@@ -43,4 +51,4 @@ async function mintNFT(tokenURI){
   });
 }
 
-mintNFT("https://gateway.pinata.cloud/ipfs/QmTtcFD3RqF4eRStSqRbYnoQ4MquEginASgrpPMzY9BadT");
+mintNFT(`https://gateway.pinata.cloud/ipfs/${IPFS_HASH_COHORT25}`);
